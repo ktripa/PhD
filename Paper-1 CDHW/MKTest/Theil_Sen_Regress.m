5c6fded99d0f48aa35ee01642c0192be263d4687
KUMAR TRIPATHY <78639856+ktripa@users.noreply.github.com>
2021-10-31 00:14:25 -0400

    Delete Paper-1 CDHW directory

--- Paper-1 CDHW/MKTest/Theil_Sen_Regress.m
+++
@@ -1,8 +0,0 @@
-function b = Theil_Sen_Regress(x,y)
-[N c]=size(x);
-Comb = combnk(1:N,2);
-deltay=diff(y(Comb),1,2);
-deltax=diff(x(Comb),1,2);
-theil=diff(y(Comb),1,2)./diff(x(Comb),1,2);
-b=median(theil);
-end
--- Paper-1 CDHW/Plot3 RP/percentage_change.m
+++
@@ -1,4 +0,0 @@
-function [val]=percentage_change(X1,X2)
-
-val=((X2-X1)./X1)*100;
-end
